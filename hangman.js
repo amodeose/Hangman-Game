5efc7fee228a01380ae26297998d2cd02c70d54a
amodeose <amodeosa@gmail.com>
2018-05-31 07:14:20 -0400

    functionality complete

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 
 //GLOBAL VARIABLES
 
-var wordBank = ["tree", "rock", "shovel", "house"];
+var wordBank = ["perseverence"];
 var word;
 var wordField = [];
 var correctLetters = 0;
@@ -32,25 +32,31 @@ generateBlanks();
 
 
 var checkLetter = function(letter) {
-    var checkedIndex = word.indexOf(letter);
-    if (checkedIndex >= 0) {
-        wordField.splice(checkedIndex, 1, letter);
-        correctLetters++;
-        if (correctLetters === word.length) {
-            wins++;
-            guesses = 5;
-            wordField = [];
-            correctLetters = 0;
-            usedLetters = [];
-            randomWord();
-            generateBlanks();
-        }
-    } else {
-        if (usedLetters.indexOf(letter) < 0) {
+    wordArr = word.split("");
+    var checkedIndex = wordArr.indexOf(letter);
+
+    if (checkedIndex < 0 && usedLetters.indexOf(letter) < 0) {
         usedLetters.push(letter);
         guesses--;
         }
+
+    while (checkedIndex >= 0) {
+        wordField.splice(checkedIndex, 1, letter);
+        correctLetters++;
+        delete wordArr[checkedIndex];
+        checkedIndex = wordArr.indexOf(letter);
     }
+
+    if (correctLetters === word.length) {
+        wins++;
+        guesses = 5;
+        wordField = [];
+        correctLetters = 0;
+        usedLetters = [];
+        randomWord();
+        generateBlanks();
+    }
+    
     document.getElementById("word").textContent = wordField.join(" ");
     document.getElementById("letters").textContent = usedLetters.join(", ");
     document.getElementById("wins").textContent = wins;
